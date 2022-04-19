f6311eff32b41f7ce617b84f44b97bf104bbb524
I-vac <ivo_nikolov00@abv.bg>
2022-04-19 16:34:09 +0300

    finished the 1st part from the tasks2 folder

--- 10-JQuery/tasks-2/Ajax.js
+++ 10-JQuery/tasks-2/Ajax.js
@@ -1,20 +1,31 @@
 $(document).ready(function(){
     $('button').click(function (event){
-        
+
+        event.preventDefault();
         let selectedValue = $('.form-control[name="difficulty"]').val();
         $.ajax({
             type: 'GET',
-            url: 'index.html',
-            data: {value: selectedValue}
+            url: `https://sugoku.herokuapp.com/board?difficulty=${selectedValue}`,
+            dataType: 'json'
         })
         .done(function (responseText){
-            $('#difficulty').html('The selected difficulty is: '+ responseText);
+           // console.log(responseText);//inicializirashta funkciq  //div elementi i 1 za red s 9 diva i 9 za kolona //onchange
+           // alert('The selected difficulty is: '+ selectedValue);
+            for (let i = 0; i < 9; i++ ){
+                    $('<div>1</div>').addClass('row').appendTo('.game-container')
+                    for (let j = 0; j < 9; j++){
+                        $('<div>2</div>').addClass('col').appendTo('.game-container .row')
+                    }
+            }
+            
+            
+
         })
         .fail(function (jqXHR , status ,error){
             alert(jqXHR.responseText)
         })
-        .always( function (){
-            $('h1:first').after('<h1>Thank you</h1>');
+        .always( function (jqXHR, status){
+            alert('The request was complete');
         });
     });  
 });

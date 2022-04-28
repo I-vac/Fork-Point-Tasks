6aeaf002eea0bab652bd6ef087c58a827fbf8771
I-vac <ivo_nikolov00@abv.bg>
2022-04-28 14:25:05 +0300

    added the numbers from the API to their corresponding places and worked on the validation

--- 10-JQuery/tasks-2/Ajax.js
+++ 10-JQuery/tasks-2/Ajax.js
@@ -9,13 +9,23 @@ $(document).ready(function(){
             dataType: 'json'
         })
         .done(function (responseText){
-           // console.log(responseText);//inicializirashta funkciq  //div elementi i 1 za red s 9 diva i 9 za kolona //onchange
+           // console.log(responseText);//inicializirashta funkciq  //div elementi i 1 za red s 9 diva i 9 za kolona //onchange v inputa//event//handle//chislata
            // alert('The selected difficulty is: '+ selectedValue);
+           //da mahna nulite// poletata da priemat samo chisla i da sa 1 simvol ot 1 do 9//input type
             for (let i = 0; i < 9; i++ ){
-                    $('<div>1</div>').addClass('row').appendTo('.game-container')
+                    let $row = $(`<div></div>`).addClass('sudokuRow')
+                    
                     for (let j = 0; j < 9; j++){
-                        $('<div>2</div>').addClass('col').appendTo('.game-container .row')
+                        $(`<div><input
+                            type="number"
+                            min="1"
+                            max="10"
+                            value="${responseText.board[i][j]}"
+                            ${!responseText.board[i][j] ? '':'disabled'} /></div>`).addClass('sudokuCol').appendTo($row)
+                      
                     }
+
+                    $row.appendTo('.game-container')
             }
             
             

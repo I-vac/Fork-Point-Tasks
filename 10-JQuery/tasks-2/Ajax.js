1761b90e7b8a52fdb6f7d28bcec405a7a18423eb
I-vac <ivo_nikolov00@abv.bg>
2022-05-09 15:08:44 +0300

    finished the sudoku taks

--- 10-JQuery/tasks-2/Ajax.js
+++ 10-JQuery/tasks-2/Ajax.js
@@ -9,26 +9,23 @@ $(document).ready(function(){
             dataType: 'json'
         })
         .done(function (responseText){
-           // console.log(responseText);//inicializirashta funkciq  //div elementi i 1 za red s 9 diva i 9 za kolona //onchange v inputa//event//handle//chislata
-           // alert('The selected difficulty is: '+ selectedValue);
-           //da mahna nulite// poletata da priemat samo chisla i da sa 1 simvol ot 1 do 9//input type
+          
             for (let i = 0; i < 9; i++ ){
+                
                     let $row = $(`<div></div>`).addClass('sudokuRow')
                     
                     for (let j = 0; j < 9; j++){
                         $(`<div><input
                             type="number"
                             min="1"
-                            max="10"
+                            max="9"
                             value="${responseText.board[i][j]}"
                             ${!responseText.board[i][j] ? '':'disabled'} /></div>`).addClass('sudokuCol').appendTo($row)
                       
                     }
-
                     $row.appendTo('.game-container')
             }
-            
-            
+          //  ${responseText.board[i][j] ? 0 : ''}
 
         })
         .fail(function (jqXHR , status ,error){

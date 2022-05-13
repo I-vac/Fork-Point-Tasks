5aa1befd48418c1dd74fb86d8192a0553a02b16d
I-vac <ivo_nikolov00@abv.bg>
2022-05-13 15:00:38 +0300

    finished task 1

---
+++ 11-RegEx/regex-string.js
@@ -0,0 +1,9 @@
+function ValidateString(string){
+    var stringREGEX = /^([a-z]{2})([_])?([A-Z]{2})?$/ ;
+    var result = stringREGEX.test(string);
+    return result;
+
+}
+console.log(ValidateString('fr_FR'))
+console.log(ValidateString('bg'))
+console.log(ValidateString('bgr'))

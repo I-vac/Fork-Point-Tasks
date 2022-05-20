5666f0547984e386194d7c828de675a7120d47ea
I-vac <ivo_nikolov00@abv.bg>
2022-05-20 12:59:20 +0300

    hopefully this is the final answer

--- 9-JS-Asynchronous/async-task2.js
+++ 9-JS-Asynchronous/async-task2.js
@@ -76,55 +76,58 @@
 //   console.log(`Start event triggered on side ${e.detail.side}`);
 // });
 // SECOND ANSWER USING ASYNC
-const up = document.getElementById('up');
-const left = document.getElementById('left');
-const right = document.getElementById('right');
-const down = document.getElementById('down');
-const box = document.getElementById('box')
+const up = document.getElementById("up");
+const left = document.getElementById("left");
+const right = document.getElementById("right");
+const down = document.getElementById("down");
+const box = document.getElementById("box");
 const step = 30;
-
-async function boxMove(){
-  return new Promise(function(resolve){
-    setTimeout(()=>{
-      resolve();
-    }, 100);
-  });
-}
-async function moveDirection(px, direction){
-  await boxMove();
-  (direction === 'top') ? 'top' : 'left';
-  document.getElementById('box').style[direction] = px + 'px';
-
+let interval;
+async function moveDirection(px, direction) {
+  direction === "top" ? "top" : "left";
+  box.style[direction] = px + "px";
 }
 
-up.addEventListener('click', function(){
-  if (box.offsetTop <= 0){
-    document.dispatchEvent(boxTop)
-    document.getElementById('box').style.top = 50 + 'px';
-  }
-  moveDirection((box.offsetTop -= step), 'top');
-})
+up.addEventListener("click", async function () {
+  clearInterval(interval);
+  interval = setInterval(function () {
+    if (box.offsetTop < 0) {
+      document.dispatchEvent(boxTop);
+      box.style.top = 20 + "px";
+      clearInterval(interval);
+    }
 
-down.addEventListener('click', function(){
-  moveDirection((box.offsetTop += step), 'top')
-})
+    moveDirection((box.offsetTop -= step), "top");
+  }, 100);
+});
 
+down.addEventListener("click", function () {
+  moveDirection((box.offsetTop += step), "top");
+});
 
-left.addEventListener('click', function(){
-  if (box.offsetLeft <= 0){
-    document.dispatchEvent(boxLeft)
-    document.getElementById('box').style.left = 50 + 'px'
-  }
-  moveDirection((box.offsetLeft -= step), 'left');
-})
+left.addEventListener("click", async function () {
+  clearInterval(interval);
+  interval = setInterval(function () {
+    if (box.offsetLeft < 0) {
+      document.dispatchEvent(boxLeft);
+      box.style.left = 25 + "px";
+      clearInterval(interval);
+    }
+    moveDirection((box.offsetLeft -= step), "left");
+  }, 100);
+});
 
-right.addEventListener('click', function(){
-  if (box.offsetLeft >= 330){
-    document.dispatchEvent(boxRight)
-    document.getElementById('box').style.right = 50 + 'px'
-  }
-  moveDirection((box.offsetLeft += step), 'left');
-})
+right.addEventListener("click", async function () {
+  clearInterval(interval);
+  interval = setInterval(function () {
+    if (box.offsetLeft > 330) {
+      document.dispatchEvent(boxRight);
+      box.style.right = 25 + "px";
+      clearInterval(interval);
+    }
+    moveDirection((box.offsetLeft += step), "left");
+  }, 100);
+});
 
 const boxTop = new CustomEvent("top", {
   detail: {
@@ -136,7 +139,6 @@ document.addEventListener("top", (e) => {
   console.log(`Start event triggered on side ${e.detail.side}`);
 });
 
-
 const boxRight = new CustomEvent("right", {
   detail: {
     side: "right",
@@ -147,7 +149,6 @@ document.addEventListener("right", (e) => {
   console.log(`Start event triggered on side ${e.detail.side}`);
 });
 
-
 const boxLeft = new CustomEvent("left", {
   detail: {
     side: "left",
@@ -157,5 +158,3 @@ const boxLeft = new CustomEvent("left", {
 document.addEventListener("left", (e) => {
   console.log(`Start event triggered on side ${e.detail.side}`);
 });
-
-
